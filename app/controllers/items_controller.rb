15ce08d45a035dacc01cbbfd08deee05da53bf65
Michael Delcambre <delcambrem@gmail.com>
2022-02-02 16:16:58 -0600

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    user = User.find_by( id: params[:user_id] )
+      if user
+        items = user.items
+      else
+        # 
+        items = Item.all
+      end
+    render json: items, include: :user, status: 404
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+      if item
+        return render json: item
+      else 
+        return render json: { error: "Not Found" }, status: :not_found 
+      end
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: 201
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
